a1195766ec0a409f1a5e851a2ce7e4dc7d6cb653
Paul Do <pdo@llu.edu>
2026-07-24 13:56:14 -0700

    started working on client.py on scraper

---
+++ backend/app/scraper/client.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb"
+
+class BannerClient:
+    def __init__(self):
+        self.session = requests.Session()
+        self.token = None
+
+    def start_session(self):
+        response = self.session.get(f"{BASE_URL}/classSearch/classSearch")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        meta_tag = soup.find('meta', attrs={'name': 'synchronizerToken'})
+        if not meta_tag:
+            raise RuntimeError("Could not find synchronizer token")
+        self.token = meta_tag['content']
+
+
+    def select_term(self, term_code: str):
+
+    def search_subject(self, subject: str, term_code: str):
