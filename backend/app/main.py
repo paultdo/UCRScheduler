825a6ee4a0b593dcd65ef2a3f4b5cd429de5a9a5
Paul Do <paultdo07@gmail.com>
2026-08-12 14:35:02 -0700

    fixed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app.include_router(schedule_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://ucrscheduler.onrender.com"],
+    allow_origins=["http://localhost:5173", "ucr-scheduler-iast0ifti-paultdos-projects.vercel.app", "https://ucr-scheduler.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
